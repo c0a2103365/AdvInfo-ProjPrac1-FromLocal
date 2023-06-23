7ed3d8033d18c98007616dadb7c3174f65875ff5
sakamoto <c0a2105787@deu.teu.ac.jp>
2023-06-23 17:54:48 +0900

    [Fix] フォルダ分けした

---
+++ ranking/mobile_r.php
@@ -0,0 +1,9 @@
+<html>
+<title>データ保存量</title>        
+<body>
+<h1>データ保存量ランキングページ</h1>
+<?php
+    
+?>
+</body>
+</html>
---
+++ ranking/ranking.php
@@ -0,0 +1,13 @@
+<?php
+    echo "<h2>ランキングページ</h2><br/>";
+    echo "<a href= ","weight_r.php"," >重さランキング</a>";
+    echo "<br><a href= ","price_r.php"," >価格ランキング</a></br>";
+    echo "<a href= ","mobile_r.php"," >データ保存量ランキング</a>";
+    echo "<br><a href= ","battery_r.php"," >稼働時間ランキング</a></br>";
+    require_once("pnwsmarty.php");
+    $pnw = new pnwsmarty();
+    $smarty = $pnw->getTpl();
+
+    $smarty->display("templates/ranking.tpl");
+
+?>
